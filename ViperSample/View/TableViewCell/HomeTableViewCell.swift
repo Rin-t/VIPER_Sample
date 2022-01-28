47194889021bd66051b70b9c79db3e680ed1290e
Rin <rin.t.4921@gmail.com>
2022-01-28 23:49:41 +0900

    tableViewCell作成

--- ViperSample/View/TableViewCell/HomeTableViewCell.swift
+++ ViperSample/View/TableViewCell/HomeTableViewCell.swift
@@ -5,4 +5,23 @@
 //  Created by Rin on 2022/01/27.
 //
 
-import Foundation
+import UIKit
+
+
+final class HomeTableViewCell: UITableViewCell {
+
+    // outlets
+    @IBOutlet private weak var titleLabel: UILabel!
+
+    // statics
+    static let identifire = "HomeTableViewCell"
+    static func nib() -> UINib {
+        UINib(nibName: identifire, bundle: nil)
+    }
+
+    // methods
+    func configure() {
+
+    }
+
+}
