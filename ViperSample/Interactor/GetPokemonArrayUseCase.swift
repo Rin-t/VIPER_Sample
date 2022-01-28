1647d06c61b182a3c7bdc932f8dfc5fe778e4b09
Rin <rin.t.4921@gmail.com>
2022-01-29 00:21:45 +0900

    UseCaseファイルの作成

---
+++ ViperSample/Interactor/GetPokemonArrayUseCase.swift
@@ -0,0 +1,12 @@
+//
+//  GetPokemonArrayUseCase.swift
+//  ViperSample
+//
+//  Created by Rin on 2022/01/29.
+//
+
+import Foundation
+
+final class GetPokemonArrayUseCase {
+    
+}
---
+++ ViperSample/Interactor/GetPokemonByIdUseCase.swift
@@ -0,0 +1,13 @@
+//
+//  GetPokemonByIdUseCase.swift
+//  ViperSample
+//
+//  Created by Rin on 2022/01/29.
+//
+
+import Foundation
+
+
+final class GetPokemonByIdUseCase {
+    
+}
